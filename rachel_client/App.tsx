f3040a223f149fa0992489bc8fb5611c8fe3e6a3
Rayson Koh <rayson.rk.koh@gmail.com>
2020-06-20 05:41:19 +0800

    Set up basic homepage, installed react-native-navigation

--- rachel_client/App.tsx
+++ rachel_client/App.tsx
@@ -1,119 +1,32 @@
-import React, {useState, useEffect} from 'react';
-import {Text, Button, SafeAreaView} from 'react-native';
-import Config from 'react-native-config';
-import auth, {FirebaseAuthTypes} from '@react-native-firebase/auth';
-import {GoogleSignin} from '@react-native-community/google-signin';
-import {LoginManager, AccessToken} from 'react-native-fbsdk';
-import FallbackTab from './components/FallbackTab';
-import QRcodeReaderTab from './components/QRcodeReaderTab';
-
-async function onFacebookButtonPress() {
-  // Attempt login with permissions
-  const result = await LoginManager.logInWithPermissions([
-    'public_profile',
-    'email',
-  ]);
-
-  if (result.isCancelled) {
-    throw 'User cancelled the login process';
-  }
-
-  // Once signed in, get the users AccesToken
-  const data = await AccessToken.getCurrentAccessToken();
-
-  if (!data) {
-    throw 'Something went wrong obtaining access token';
-  }
-
-  // Create a Firebase credential with the AccessToken
-  const facebookCredential = auth.FacebookAuthProvider.credential(
-    data.accessToken,
-  );
-
-  // Sign-in the user with the credential
-  return auth().signInWithCredential(facebookCredential);
-}
-
-GoogleSignin.configure({
-  webClientId: Config.WEB_CLIENT_ID,
-});
-
-async function onGoogleButtonPress() {
-  // Get the users ID token
-  await GoogleSignin.signIn();
-  const {idToken, accessToken} = await GoogleSignin.getTokens();
-
-  // Create a Google credential with the token
-  const googleCredential = auth.GoogleAuthProvider.credential(
-    idToken,
-    accessToken,
-  );
-
-  // Sign-in the user with the credential
-  return auth().signInWithCredential(googleCredential);
-}
+import React from 'react';
+import {SafeAreaView, StyleSheet} from 'react-native';
+import {Text} from 'react-native-elements';
 
 const App = () => {
-  // Set an initializing state whilst Firebase connects
-  const [initializing, setInitializing] = useState(true);
-  const [user, setUser] = useState<FirebaseAuthTypes.User | null>(null);
-
-  // Handle user state changes
-  function onAuthStateChanged(user: FirebaseAuthTypes.User | null) {
-    setInitializing(true);
-    setUser(user);
-    setInitializing(false);
-  }
-
-  useEffect(() => {
-    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
-    return subscriber; // unsubscribe on unmount
-  }, []);
-
-  const renderContents = () => {
-    if (initializing) {
-      return <Text>Initializing...</Text>;
-    } else if (!user) {
-      return (
-        <>
-          <Button
-            title="Google sign-in"
-            onPress={() => {
-              onGoogleButtonPress().then(() =>
-                console.log('Signed in with Google!'),
-              );
-            }}
-          />
-          <Button
-            title="Facebook sign-in"
-            onPress={() => {
-              onFacebookButtonPress().then(() =>
-                console.log('Signed in with Facebook!'),
-              );
-            }}
-          />
-        </>
-      );
-    } else {
-      return <Text>Welcome {user.displayName}!</Text>;
-    }
-  };
-
   return (
-    <SafeAreaView>
-      {renderContents()}
-      <Button
-        title="Sign out"
-        onPress={() =>
-          auth()
-            .signOut()
-            .then(() => console.log('successfully signed out!'))
-        }
-      />
-      <FallbackTab />
-      <QRcodeReaderTab />
-    </SafeAreaView>
+    <>
+      <SafeAreaView>
+        <Text h1 style={styles.title}>
+          Hello World!
+        </Text>
+      </SafeAreaView>
+    </>
   );
 };
 
 export default App;
+
+const styles = StyleSheet.create({
+  title: {
+    marginTop: 16,
+    paddingVertical: 8,
+    borderWidth: 4,
+    borderColor: '#20232a',
+    borderRadius: 6,
+    backgroundColor: '#61dafb',
+    color: '#20232a',
+    textAlign: 'center',
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
+});
