1114b7a84e754b5fff45d11d38a978a200f83c47
Rayson Koh <rayson.rk.koh@gmail.com>
2020-06-20 07:07:52 +0800

    Revert "Modified android settings to get react-native-navigation working on android"
    This reverts commit 6153e9ddb0ac20de9afd46140d860c003f98e7f5.

--- rachel_client/index.js
+++ rachel_client/index.js
@@ -5,7 +5,7 @@ import {Navigation} from 'react-native-navigation';
 import App from './App';
 import {name as appName} from './app.json';
 
-Navigation.registerComponent(`com.${appName}.HomePage`, () => App);
+Navigation.registerComponent(`com.${appName}.WelcomeScreen`, () => App);
 Navigation.events().registerAppLaunchedListener(() => {
   Navigation.setRoot({
     root: {
@@ -13,7 +13,7 @@ Navigation.events().registerAppLaunchedListener(() => {
         children: [
           {
             component: {
-              name: `com.${appName}.HomePage`,
+              name: `com.${appName}.WelcomeScreen`,
             },
           },
         ],
