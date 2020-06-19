ea20695defc2f1fcb568d1adca068de66b08b506
Rayson Koh <rayson.rk.koh@gmail.com>
2020-06-20 07:08:24 +0800

    Revert "Successfully installed react-native-navigation"
    This reverts commit 7c54f6269cd5f4c7dda0c38d2d8a483d912c9f03.

--- rachel_client/index.js
+++ rachel_client/index.js
@@ -1,23 +1,9 @@
 /**
  * @format
  */
-import {Navigation} from 'react-native-navigation';
+
+import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
-Navigation.registerComponent(`com.${appName}.WelcomeScreen`, () => App);
-Navigation.events().registerAppLaunchedListener(() => {
-  Navigation.setRoot({
-    root: {
-      stack: {
-        children: [
-          {
-            component: {
-              name: `com.${appName}.WelcomeScreen`,
-            },
-          },
-        ],
-      },
-    },
-  });
-});
+AppRegistry.registerComponent(appName, () => App);
