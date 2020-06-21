1fd2ddd07ef44227040874305792749312375917
Rayson Koh <rayson.rk.koh@gmail.com>
2020-06-21 14:59:16 +0800

    Created customAxios with custom base url

--- rachel_client/pages/Dashboard.tsx
+++ rachel_client/pages/Dashboard.tsx
@@ -1,17 +1,36 @@
-import React, {useContext} from 'react';
+import React, {useState, useEffect, useContext} from 'react';
 import {AuthContext} from '../contexts/AuthContext';
 import {AuthContextType} from '../types/contextTypes';
 import {SafeAreaView} from 'react-native';
 import {Text} from 'react-native-elements';
 import {DashboardScreenProps} from 'types/screenTypes';
+import customAxios from '../helpers/customAxios';
 
 const Dashboard = ({route, navigation}: DashboardScreenProps) => {
   const auth: AuthContextType = useContext(AuthContext);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    customAxios
+      .post('auth/login', {
+        userid: auth.user?.uid,
+      })
+      .then((res) => {
+        console.log(res.data);
+        setIsLoading(false);
+      });
+  }, []);
 
   return (
     <SafeAreaView>
-      <Text h1>This is the Dashboard page</Text>
-      <Text>{auth.user ? auth.user.displayName : 'NULL USER'}</Text>
+      {isLoading ? (
+        <Text>LOADING NOW</Text>
+      ) : (
+        <>
+          <Text h1>This is the Dashboard page</Text>
+          <Text>{auth.user ? auth.user.displayName : 'NULL USER'}</Text>
+        </>
+      )}
     </SafeAreaView>
   );
 };
