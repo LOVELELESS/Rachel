632334c3019506057de72ef71f01efc2d11ac87c
Rayson Koh <rayson.rk.koh@gmail.com>
2020-06-25 00:18:32 +0800

    Added addMeetings button in dashboardpage

--- rachel_client/pages/Dashboard.tsx
+++ rachel_client/pages/Dashboard.tsx
@@ -1,26 +1,66 @@
-import React, {useContext} from 'react';
+import React, {useState, useEffect, useContext} from 'react';
+import {StyleSheet, ScrollView} from 'react-native';
 import {DashboardScreenProps} from '../types/screenTypes';
 import {AuthContext} from '../contexts/AuthContext';
 import {AuthContextType} from '../types/contextTypes';
+import customAxios from '../helpers/customAxios';
 import {SafeAreaView} from 'react-native';
-import {Text} from 'react-native-elements';
+import {Text, Button} from 'react-native-elements';
+import {Footer, FooterTab, Header, Content, Container} from 'native-base';
 import MeetingCard from '../components/MeetingCard';
 
 const Dashboard = ({route, navigation}: DashboardScreenProps) => {
+  const [meetings, setMeetings] = useState<Array<Object>>([]);
   const auth: AuthContextType = useContext(AuthContext);
 
+  useEffect(() => {
+    customAxios
+      .get(
+        `workspaces/${auth.userSettings.workspaceName}/users/${auth.user.uid}/meetings/`,
+      )
+      .then((res) => {
+        setMeetings(res.data.meetings);
+      });
+  });
+
+  const renderMeetingCards = () => {
+    if (meetings.length < 1) {
+      return <Text h1>NO PENDING MEETINGS</Text>;
+    } else {
+      return meetings.map((meeting) => (
+        <MeetingCard
+          data={{
+            meetingTitle: meeting.title,
+            meetingDate: meeting.date,
+            meetingDescription: meeting.description,
+            meetingId: meeting.meetingid,
+          }}
+        />
+      ));
+    }
+  };
+
   return (
-    <SafeAreaView>
-      <Text h1>This is the Dashboard page</Text>
-      <MeetingCard
-        data={{
-          meetingTitle: 'title',
-          meetingDate: Date.now(),
-          meetingDescription: 'description',
-          meetingId: '12345',
-        }}
-      />
-    </SafeAreaView>
+    <Container>
+      <Header>
+        <Text h1>This is the Dashboard page</Text>
+      </Header>
+      <Content>
+        <MeetingCard
+          data={{
+            meetingTitle: 'title',
+            meetingDate: 'some date, later change',
+            meetingDescription: 'description',
+            meetingId: '12345',
+          }}
+        />
+        {renderMeetingCards()}
+      </Content>
+
+      <Footer>
+        <Button title="add meetings" />
+      </Footer>
+    </Container>
   );
 };
 
