d453757e2796277aeffad98e733c151f5caa591b
Rayson Koh <rayson.rk.koh@gmail.com>
2020-06-30 03:53:26 +0800

    Updated stylings for SignInPage

--- rachel_client/App.tsx
+++ rachel_client/App.tsx
@@ -1,16 +1,8 @@
 import 'react-native-gesture-handler';
 import React, {useEffect} from 'react';
-import {Alert} from 'react-native';
 import {AuthProvider} from './contexts/AuthContext';
 import AppNavigation from './AppNavigation';
-import FontAwesomeIcons from 'react-native-vector-icons/FontAwesome';
-import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
-import Ionicons from 'react-native-vector-icons/Ionicons';
 import messaging from '@react-native-firebase/messaging';
-
-FontAwesomeIcons.loadFont();
-MaterialIcons.loadFont();
-Ionicons.loadFont();
 
 const App = () => {
   useEffect(() => {
--- rachel_client/pages/SigninPage.tsx
+++ rachel_client/pages/SigninPage.tsx
@@ -1,13 +1,14 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import Config from 'react-native-config';
-import {SigninPageScreenProps} from '../types/screenTypes';
-import {SafeAreaView, View, StyleSheet} from 'react-native';
-import {Text, SocialIcon, Button} from 'react-native-elements';
 import {GoogleSignin} from '@react-native-community/google-signin';
 import {
   onGoogleButtonPress,
   onFacebookButtonPress,
 } from '../helpers/AuthHelpers';
+import {SigninPageScreenProps} from '../types/screenTypes';
+import {SafeAreaView, View, StyleSheet} from 'react-native';
+import {Text, SocialIcon, Icon} from 'react-native-elements';
+import {TouchableOpacity} from 'react-native-gesture-handler';
 
 GoogleSignin.configure({
   webClientId: Config.WEB_CLIENT_ID,
@@ -16,33 +17,51 @@ GoogleSignin.configure({
 const SigninPage = ({route, navigation}: SigninPageScreenProps) => {
   return (
     <SafeAreaView style={styles.container}>
-      <Text h4 style={styles.title}>
-        Welcome to Rachel!
-      </Text>
-      <Text style={styles.subtitle1}>The E-Receptionist</Text>
-      <View style={styles.dummy} />
-      <Text style={styles.subtitle2}>
-        Please sign in to either your Google or Facebook account to continue
-      </Text>
-      <View style={styles.loginButtons}>
-        <SocialIcon
-          title="Sign In With Google"
-          button
-          style={styles.button}
-          onPress={onGoogleButtonPress}
-          type="google"
+      <View>
+        <Text h2 style={styles.title}>
+          Welcome to Rachel!
+        </Text>
+        <Text h4 style={styles.subtitle1}>
+          The E-Receptionist
+        </Text>
+      </View>
+      <TouchableOpacity
+        onPress={() => navigation.navigate('ReceptionistWelcomePage')}>
+        <Icon
+          type="font-awesome-5"
+          name="concierge-bell"
+          size={100}
+          solid
+          color={'#FF69B4'}
+          style={styles.icon}
+          containerStyle={styles.iconContainer}
+          reverse
+          raised
         />
-        <SocialIcon
-          title="Sign In With Facebook"
-          button
-          style={styles.button}
-          onPress={onFacebookButtonPress}
-          type="facebook"
-        />
-        <Button
-          title="Launch E-Receptionist"
-          onPress={() => navigation.navigate('ReceptionistWelcomePage')}
-        />
+        <Text h3 style={styles.subtitle1}>
+          Launch E-Receptionist!
+        </Text>
+      </TouchableOpacity>
+      <View>
+        <Text h4 style={styles.subtitle2}>
+          Please sign in to access your account:
+        </Text>
+        <View style={styles.loginButtons}>
+          <SocialIcon
+            title="Sign In With Google"
+            button
+            style={styles.button}
+            onPress={onGoogleButtonPress}
+            type="google"
+          />
+          <SocialIcon
+            title="Sign In With Facebook"
+            button
+            style={styles.button}
+            onPress={onFacebookButtonPress}
+            type="facebook"
+          />
+        </View>
       </View>
     </SafeAreaView>
   );
@@ -51,28 +70,23 @@ const SigninPage = ({route, navigation}: SigninPageScreenProps) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'space-around',
   },
-  dummy: {
-    height: '25%',
+  iconContainer: {
+    alignSelf: 'center',
   },
-  loginButtons: {
-    flex: 1,
-    justifyContent: 'flex-start',
-  },
+  icon: {},
+  loginButtons: {},
   title: {
     textAlign: 'center',
-    padding: 20,
     paddingBottom: 10,
   },
   subtitle1: {
     textAlign: 'center',
-    fontSize: 22,
-    padding: 20,
   },
   subtitle2: {
     textAlign: 'center',
-    padding: 10,
-    fontSize: 24,
+    paddingBottom: 10,
   },
   button: {
     margin: 10,
