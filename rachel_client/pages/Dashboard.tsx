f9aa1e17a3ce2e842ff66519acb6d66a974d5c2f
Rayson Koh <rayson.rk.koh@gmail.com>
2020-06-30 04:32:52 +0800

    Added basic stylings for DashboardPage

--- rachel_client/pages/Dashboard.tsx
+++ rachel_client/pages/Dashboard.tsx
@@ -1,12 +1,12 @@
 import React, {useState, useEffect, useContext} from 'react';
-import {StyleSheet, ScrollView} from 'react-native';
+import {ScrollView, View, StyleSheet} from 'react-native';
 import {useFocusEffect} from '@react-navigation/native';
 import {DashboardScreenProps} from '../types/screenTypes';
 import {AuthContext} from '../contexts/AuthContext';
 import {AuthContextType} from '../types/contextTypes';
 import customAxios from '../helpers/customAxios';
 import {SafeAreaView} from 'react-native';
-import {Text, Button} from 'react-native-elements';
+import {Text, Button, Icon} from 'react-native-elements';
 import {Footer, FooterTab, Header, Content, Container} from 'native-base';
 import MeetingCard from '../components/MeetingCard';
 import {deepCopy} from '../helpers/arrayUtils';
@@ -31,7 +31,13 @@ const Dashboard = ({route, navigation}: DashboardScreenProps) => {
 
   const renderMeetingCards = () => {
     if (meetings.length < 1) {
-      return <Text h1>NO PENDING MEETINGS</Text>;
+      return (
+        <View>
+          <Text h4 style={styles.noMeetingsText}>
+            You have no meetings scheduled! :>
+          </Text>
+        </View>
+      );
     } else {
       return meetings.map((meeting, i) => (
         <MeetingCard
@@ -55,19 +61,44 @@ const Dashboard = ({route, navigation}: DashboardScreenProps) => {
   };
 
   return (
-    <Container>
-      <Header>
-        <Text h1>This is the Dashboard page</Text>
-      </Header>
-      <Content>{renderMeetingCards()}</Content>
-      <Footer>
-        <Button
-          title="add meetings"
-          onPress={() => navigation.navigate('AddOrEditMeetingPage')}
-        />
-      </Footer>
-    </Container>
+    <View style={styles.container}>
+      <Text h2 style={styles.header}>
+        Welcome, {auth.user?.displayName}!
+      </Text>
+      <ScrollView>{renderMeetingCards()}</ScrollView>
+      <Button
+        style={styles.addMeeting}
+        buttonStyle={styles.addMeetingButton}
+        titleStyle={styles.addMeetingTitle}
+        title="Add Meeting"
+        onPress={() => navigation.navigate('AddOrEditMeetingPage')}
+        icon={{name: 'group-add', size: 30, color: 'white'}}
+      />
+    </View>
   );
 };
 
 export default Dashboard;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  header: {
+    textAlign: 'center',
+    padding: 20,
+  },
+  noMeetingsText: {
+    padding: 20,
+  },
+  addMeeting: {
+    padding: 30,
+  },
+  addMeetingButton: {
+    backgroundColor: '#0074d9',
+  },
+  addMeetingTitle: {
+    fontWeight: 'bold',
+    padding: 5,
+  },
+});
