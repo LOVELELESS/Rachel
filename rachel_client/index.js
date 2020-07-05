fa41a953687f84746ba0f38d9eb912f5c5dda621
Rayson Koh <rayson.rk.koh@gmail.com>
2020-07-05 17:49:42 +0800

    Add rn imports in index.js to prevent potential require cycles

--- rachel_client/index.js
+++ rachel_client/index.js
@@ -2,6 +2,10 @@
  * @format
  */
 
+import 'react-native-gesture-handler';
+import '@react-navigation/native';
+import '@react-navigation/stack';
+import '@react-navigation/drawer';
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
